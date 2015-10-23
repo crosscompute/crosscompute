d2e04bef45da1363c0c785f79c56a6adcffc972b
Roy Hyunjin Han <rhh@crosscompute.com>
2015-10-23 02:06:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='crosscompute',
-    version='0.2.1',
+    version='0.2.2',
     description='Publish your computational model',
     long_description=DESCRIPTION,
     classifiers=[
