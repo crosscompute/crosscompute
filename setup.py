4da0027e0b6ee1ebca3087ed44b29dfc676a09d7
Roy Hyunjin Han <rhh@crosscompute.com>
2015-10-23 01:06:33 -0400

    Fix PyPI classifier

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     long_description=DESCRIPTION,
     classifiers=[
         'Programming Language :: Python',
-        'Framework :: Pyramid :: CrossCompute',
+        'Framework :: Pyramid',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
