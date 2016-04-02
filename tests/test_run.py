8a7847403e5e66f6c1d21bd0dd3945b4186d3abd
AShoydokova <ashoydokova@yahoo.com>
2016-04-02 13:36:17 -0400

    Fix #2

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 from conftest import TOOL_FOLDER
 from crosscompute.tests import run
 from string import digits, letters
@@ -14,3 +14,11 @@ def test_print_unicode():
     standard_output = run(TOOL_FOLDER, 'print-unicode')[0]
     assert 'standard_outputs.a = клубника' in standard_output
     assert 'standard_errors.b = малина' in standard_output
+
+def test_accept_script_filename_with_spaces():
+    standard_output = run(
+        TOOL_FOLDER, 'accept-script-filename-with-spaces-in-single-quotes')[0]
+    assert 'abc = xyz' in standard_output
+    standard_output = run(
+        TOOL_FOLDER, 'accept-script-filename-with-spaces-in-double-quotes')[0]
+    assert 'abc = xyz' in standard_output
