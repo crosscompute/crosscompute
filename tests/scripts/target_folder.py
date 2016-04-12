ce210775795390561a760e27dd06563c95945348
AShoydokova <ashoydokova@yahoo.com>
2016-04-12 16:18:58 -0400

    adding a test to check if output files are stored in target_folder

---
+++ tests/scripts/target_folder.py
@@ -0,0 +1,4 @@
+from sys import argv
+
+target_folder = argv[1]
+print ('target folder = ' + target_folder)
--- tests/test_run.py
+++ tests/test_run.py
@@ -2,6 +2,7 @@
 from conftest import TOOL_FOLDER
 from crosscompute.tests import run
 from string import digits, letters
+import re
 
 
 def test_capture_standard_output():
@@ -22,3 +23,18 @@ def test_accept_script_filename_with_spaces():
     standard_output = run(
         TOOL_FOLDER, 'accept-script-filename-with-spaces-in-double-quotes')[0]
     assert 'abc = xyz' in standard_output
+
+def test_correct_target_folder_path():
+    standard_output = run(
+        TOOL_FOLDER, 'show-correct-target_folder')[0]
+    target_folder = re.search(r'\ntarget_folder\s=\s(.+?)\r',standard_output).group(1)
+    """
+    # how it is now, the test will pass
+    check = 'standard_outputs.target folder = ' + 'tmpshow-correct-target_folderresults'
+    """
+    # should either this
+    check = 'standard_outputs.target folder = ' + target_folder
+    # or this
+    check = 'standard_outputs.target folder = ' + target_folder + '/tmpshow-correct-target_folderresults'
+    #assert re.match(r'target folder = tmpshow-correct-target_folderresults',standard_output) is not None  
+    assert check in standard_output
