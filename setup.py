2cb46e27820f9748ce9b6731b549a152f44bef7d
Roy Hyunjin Han <rhh@crosscompute.com>
2016-05-22 18:18:11 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='crosscompute',
-    version='0.5.0',
+    version='0.5.1',
     description='Publish your own tools by writing a configuration file',
     long_description=DESCRIPTION,
     classifiers=[
