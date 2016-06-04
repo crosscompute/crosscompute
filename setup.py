7a49235864c1130fd9009df40891bec179a2d25d
Roy Hyunjin Han <rhh@crosscompute.com>
2016-06-03 17:20:46 -0700

    Start work on 0.5.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='crosscompute',
-    version='0.5.3.1',
+    version='0.5.4',
     description='Publish your own tools by writing a configuration file',
     long_description=DESCRIPTION,
     classifiers=[
