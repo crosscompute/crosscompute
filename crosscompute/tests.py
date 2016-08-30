700c416ed04c6d80afc5addbf11798ef0129d3ea
Roy Hyunjin Han <rhh@crosscompute.com>
2016-08-29 20:13:52 -0400

    Fix tests

--- crosscompute/tests.py
+++ crosscompute/tests.py
@@ -21,9 +21,8 @@ def serve(data_folder, tool_name, result_arguments=None):
     initialize_data_types()
     response, client = _prepare_response(
         data_folder, tool_name, result_arguments)
-    assert response.status_code == 303, response.data
-    result_url = parse_url(dict(response.headers)['Location']).path
-    with client.get(result_url) as response:
+    result_url = json.loads(response.data.decode('utf-8'))['result_url']
+    with client.get(parse_url(result_url).path) as response:
         soup = BeautifulSoup(response.data, 'html.parser')
     return soup, client
 
@@ -39,5 +38,5 @@ def _prepare_response(data_folder, tool_name, result_arguments):
     tool_definition = prepare_tool_definition(tool_name)
     app = get_app(tool_definition, data_folder, 'Test', 'Python')
     client = Client(app, BaseResponse)
-    with client.post('/t/1', data=result_arguments or {}) as response:
+    with client.post('/t/1.json', data=result_arguments or {}) as response:
         return response, client
