84ddc4248db90ce090dccb6d685703afe143b66f
Roy <rhh@crosscompute.com>
2016-12-22 10:08:48 -0500

    Add travis configuration

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ cc-python = crosscompute.scaffolds:PythonToolTemplate
 
 
 REQUIREMENTS = [
-    'invisibleroads-macros>=0.7.4',
+    'invisibleroads-macros>=0.7.5',
     'invisibleroads-posts>=0.5.2',
-    'invisibleroads-uploads>=0.1.0',
+    'invisibleroads-uploads>=0.1.1',
     'invisibleroads>=0.1.7',
     'markupsafe',
     'mistune',
@@ -40,7 +40,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='crosscompute',
-    version='0.6.1',
+    version='0.6.2',
     description='Publish your own tools by writing a configuration file',
     long_description=DESCRIPTION,
     classifiers=[
