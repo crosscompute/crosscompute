bdc57cf434cfd378803e1b0e085cf2e5f539464c
Roy <rhh@crosscompute.com>
2017-03-02 00:37:18 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ cc-python = crosscompute.scaffolds:PythonToolTemplate
 
 
 REQUIREMENTS = [
-    'invisibleroads-macros>=0.8.1',
-    'invisibleroads-posts>=0.5.4',
-    'invisibleroads-uploads>=0.2.2',
+    'invisibleroads-macros>=0.8.1.1',
+    'invisibleroads-posts>=0.5.4.1',
+    'invisibleroads-uploads>=0.2.3',
     'invisibleroads>=0.1.7',
     'markupsafe',
     'mistune',
