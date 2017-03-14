23bfead091719bb766e06da4f35a7b4d276ff0ed
salah ahmed <salah.ahmed.hslc@gmail.com>
2017-03-14 11:52:03 -0400

    template_parts (#21)
    * change if statement to more intuitive test
    
    * avoid exceptions for testing class
    
    * strip all members of list
    
    * Update serve.py
    
    remove commented-out code

--- crosscompute/scripts/serve.py
+++ crosscompute/scripts/serve.py
@@ -283,18 +283,16 @@ def parse_template_parts(template_text, data_items):
     content = MARKDOWN_TITLE_PATTERN.sub('', template_text).strip()
     parts = []
     data_item_by_key = {x.key: x for x in data_items}
+    variables = [x.strip() for x in ARGUMENT_NAME_PATTERN.findall(content)]
     for index, x in enumerate(ARGUMENT_NAME_PATTERN.split(content)):
         x = x.strip()
         if not x:
             continue
-        if index % 2 == 1:
+        if x in variables:
             key, _, name = x.partition(':')
             key = key.strip()
-            try:
-                x = data_item_by_key[key]
-            except KeyError:
-                x = '{ %s }' % x
-            else:
+            x = data_item_by_key.get(key, '{ %s }' % x)
+            if isinstance(x, DataItem):
                 name = name.strip()
                 if name:
                     x.name = name
