3fd520131577f41f9eb2df27c9598ad3c7a29d66
salah <salah93@crosscompute.com>
2017-03-14 15:12:51 +0000

    strip all members of list

--- crosscompute/scripts/serve.py
+++ crosscompute/scripts/serve.py
@@ -283,7 +283,7 @@ def parse_template_parts(template_text, data_items):
     content = MARKDOWN_TITLE_PATTERN.sub('', template_text).strip()
     parts = []
     data_item_by_key = {x.key: x for x in data_items}
-    variables = ARGUMENT_NAME_PATTERN.findall(content)
+    variables = [x.strip() for x in ARGUMENT_NAME_PATTERN.findall(content)]
     for index, x in enumerate(ARGUMENT_NAME_PATTERN.split(content)):
         x = x.strip()
         if not x:
