9063a0d6367a1ce8fcc4aeefb20a120d37041f5a
Roy <rhh@crosscompute.com>
2017-10-27 17:53:48 -0400

    Prepare for release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ cc-python = crosscompute.scaffolds:PythonToolTemplate
 
 
 REQUIREMENTS = [
-    'invisibleroads-macros>=0.8.4.5',
+    'invisibleroads-macros>=0.9.0',
     'invisibleroads>=0.2.0',
     'invisibleroads-posts>=0.5.6',
     'invisibleroads-uploads>=0.4.0',
@@ -67,5 +67,6 @@ setup(
         'pytest',
         'pytest-mock',
         'werkzeug',
+        'webob',
     ],
     entry_points=ENTRY_POINTS)
