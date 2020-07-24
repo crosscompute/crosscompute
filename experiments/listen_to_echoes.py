979691626621f7748f9e0b28e98c7e633b1d0673
Roy Hyunjin Han <rhh@crosscompute.com>
2020-07-24 05:18:10 +0000

    Save experiments

--- experiments/listen_to_echoes.py
+++ experiments/listen_to_echoes.py
@@ -1,17 +1,15 @@
 import os
 from sseclient import SSEClient
-import requests
 
-url = os.environ.get('CROSSCOMPUTE_SERVICES_URL')
+services_url = os.environ.get('CROSSCOMPUTE_SERVICES_URL')
 echo_token = os.environ.get('CROSSCOMPUTE_ECHO_TOKEN')
-task_token = os.environ.get('CROSSCOMPUTE_TASK_TOKEN')
-echo_headers = {'Authorization': echo_token}
-task_headers = {'Authorization': task_headers}
+chore_token = os.environ.get('CROSSCOMPUTE_CHORE_TOKEN')
+echo_headers = {'Authorization': 'Bearer ' + echo_token}
+chore_headers = {'Authorization': 'Bearer ' + chore_token}
 
 
-msgs = SSEClient(url, headers=echo_headers)
-msg_list = [m for m in msgs]
-res = requests.get(url, task_headers)
-print(res.content)
-    
-        
+echoes_url = services_url + '/echoes.json'
+messages = SSEClient(echoes_url, headers=echo_headers)
+for message in messages:
+    print(message.__dict__)
+    print(message)
