51c88ff5b8a09806005edac92fe909fe5661a4d7
Roy Hyunjin Han <rhh@crosscompute.com>
2020-07-27 14:49:59 +0000

    Explore message format

--- experiments/listen_to_echoes.py
+++ experiments/listen_to_echoes.py
@@ -1,15 +1,36 @@
-import os
+from os import environ
 from sseclient import SSEClient
 
-services_url = os.environ.get('CROSSCOMPUTE_SERVICES_URL')
-echo_token = os.environ.get('CROSSCOMPUTE_ECHO_TOKEN')
-chore_token = os.environ.get('CROSSCOMPUTE_CHORE_TOKEN')
-echo_headers = {'Authorization': 'Bearer ' + echo_token}
-chore_headers = {'Authorization': 'Bearer ' + chore_token}
+
+services_url = environ['CROSSCOMPUTE_SERVICES_URL']
+# Get echoes token
+echoes_token = environ['CROSSCOMPUTE_ECHOES_TOKEN']
+# Get chores token
+chores_token = environ['CROSSCOMPUTE_CHORES_TOKEN']
 
 
 echoes_url = services_url + '/echoes.json'
-messages = SSEClient(echoes_url, headers=echo_headers)
+headers = {'Authorization': 'Bearer ' + echoes_token}
+echoes_client = SSEClient(echoes_url, headers=headers)
+
+
+# For each echo,
+for message in echoes_client:
+    print(message.__dict__)
+    print(message.__dict__.keys())
+    print(message.id)
+    print(message.event)
+    print(message.data)
+    print(message.retry)
+    # Get chore
+    # Sleep
+    # Send update
+
+
+'''
+chore_headers = {'Authorization': 'Bearer ' + chore_token}
+messages =
 for message in messages:
     print(message.__dict__)
     print(message)
+'''
