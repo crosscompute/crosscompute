62be7442f2b2fc57d97bcd00303a9981f6187693
Roy Hyunjin Han <rhh@crosscompute.com>
2020-08-08 08:01:26 -0400

    Save notes

---
+++ experiments/add_tool.py
@@ -0,0 +1,22 @@
+import requests
+# from argparse import ArgumentParser
+
+
+token = 'abc'
+headers = {
+    'Authorization': 'Bearer ' + token,
+}
+configuration_path = '~/Projects/crosscompute-platform/crosscompute-examples/add-numbers/.crosscompute.yml'
+base_url = 'https://services.projects.crosscompute.com'
+url = base_url + '/tools.json'
+
+
+response = requests.post(url, headers=headers, json={
+    'configurationText': 'hey',
+})
+d = response.json()
+print(d['id'])
+
+
+if __name__ == '__main__':
+    pass
