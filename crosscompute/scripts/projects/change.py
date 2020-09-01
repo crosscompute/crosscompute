20a9073690fdc7424fec66d4d4dcb885e1ff3451
Roy Hyunjin Han <rhh@crosscompute.com>
2020-09-01 15:58:39 -0400

    Replace project name if specified

--- crosscompute/scripts/projects/change.py
+++ crosscompute/scripts/projects/change.py
@@ -11,6 +11,7 @@ class ChangeProjectScript(LoggingScript):
     def configure(self, argument_subparser):
         super().configure(argument_subparser)
         argument_subparser.add_argument('projectId', metavar='PROJECT-ID')
+        argument_subparser.add_argument('--name')
         argument_subparser.add_argument('--toolId', action='append')
         argument_subparser.add_argument('--datasetId', action='append')
         argument_subparser.add_argument('--resultId', action='append')
@@ -20,14 +21,30 @@ class ChangeProjectScript(LoggingScript):
         host = get_crosscompute_host()
         token = get_crosscompute_token()
         project_id = getattr(args, 'projectId')
+        project_name = args.name
         tool_ids = args.toolId or []
         dataset_ids = args.datasetId or []
         result_ids = args.resultId or []
-        d = run(host, token, project_id, tool_ids, dataset_ids, result_ids)
+        d = run(
+            host,
+            token,
+            project_id,
+            project_name,
+            tool_ids,
+            dataset_ids,
+            result_ids)
         return d
 
 
-def run(host, token, project_id, tool_ids, dataset_ids, result_ids):
+def run(
+    host,
+    token,
+    project_id,
+    project_name=None,
+    tool_ids=[],
+    dataset_ids=[],
+    result_ids=[],
+):
     url = f'{host}/projects/{project_id}.json'
     headers = {'Authorization': 'Bearer ' + token}
     d = {
@@ -35,5 +52,7 @@ def run(host, token, project_id, tool_ids, dataset_ids, result_ids):
         'datasetIds': dataset_ids,
         'resultIds': result_ids,
     }
+    if project_name:
+        d['name'] = project_name
     response = requests.patch(url, headers=headers, json=d)
     return response.json()
