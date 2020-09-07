8a17898b6fbf92c6000bfae65896f3db953422b7
Salah Ahmed <salahs.email@pm.me>
2020-09-07 18:32:43 -0400

    add test for loading config file

---
+++ tests/test_routines.py
@@ -0,0 +1,28 @@
+from crosscompute.routines import load_tool_configuration
+
+
+def test_load_tool_configuration(config_file):
+    normalized_tool_configuration = load_tool_configuration(config_file)
+    assert {
+        "id": "add-numbers",
+        "input": {
+            "templates": [
+                {"blocks": [{"id": "a"}, {"id": "b"}], "id": "generated", "name": "Generated"}
+            ],
+            "variables": [
+                {"id": "a", "name": "a", "path": "/tmp/a.txt", "view": "number"},
+                {"id": "b", "name": "b", "path": "/tmp/b.txt", "view": "number"},
+            ],
+        },
+        "name": "Add Two Numbers",
+        "output": {
+            "templates": [
+                {"blocks": [{"id": "s"}], "id": "generated", "name": "Generated"}
+            ],
+            "variables": [
+                {"id": "s", "name": "s", "path": "/tmp/s.txt", "view": "number"}
+            ],
+        },
+        "slug": "add",
+        "version": "0.1.0",
+    } == normalized_tool_configuration
