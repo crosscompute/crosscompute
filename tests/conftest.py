c752ebae50269825c30533e2f0a0c373b836f7a2
Salah Ahmed <salahs.email@pm.me>
2020-09-19 21:54:29 -0400

    add test for templates

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,3 +36,64 @@ def config_file():
         )
         temp.flush()
         yield temp.name
+
+
+@pytest.fixture
+def config_file_with_templates():
+    with tempfile.NamedTemporaryFile("w") as temp:
+        temp.write(
+            dedent(
+                """
+            protocol: 0.8.3
+            name: Add Numbers
+            version:
+                name: 0.2.0
+            input:
+                variables:
+                    - id: a
+                      name: A
+                      view: number
+                      path: numbers.json
+                    - id: b
+                      name: B
+                      view: number
+                      path: numbers.json
+                templates:
+                    - id: basic
+                      name: Basic
+                      blocks:
+                        - id: template-a
+
+            output:
+                variables:
+                    - id: c
+                      name: C
+                      view: number
+                      path: sum.json
+                templates:
+                    - id: summary
+                      name: Summary
+                      blocks:
+                        - view: markdown
+                          data:
+                            value: 5
+            tests:
+                - id: integers
+                  name: Add Integers
+                  path: tests/integers
+                - id: floats
+                  name: Add Floats
+                  path: tests/floats
+            script:
+                uri: git@github.com:crosscompute/crosscompute-examples
+                folder: add-numbers
+                command: bash run.sh
+            environment:
+                image: docker.io/library/python:slim-buster
+                processor: cpu
+                memory: tiny
+            """
+            )
+        )
+        temp.flush()
+        yield temp.name
--- tests/test_routines.py
+++ tests/test_routines.py
@@ -1,13 +1,17 @@
 import crosscompute.routines as cc
 
 
-def test_load_tool_configuration(config_file):
+def test_load_tool_configuration_basic(config_file):
     normalized_tool_configuration = cc.load_tool_configuration(config_file)
     assert {
         "id": "add-numbers",
         "input": {
             "templates": [
-                {"blocks": [{"id": "a"}, {"id": "b"}], "id": "generated", "name": "Generated"}
+                {
+                    "blocks": [{"id": "a"}, {"id": "b"}],
+                    "id": "generated",
+                    "name": "Generated",
+                }
             ],
             "variables": [
                 {"id": "a", "name": "a", "path": "/tmp/a.txt", "view": "number"},
@@ -25,4 +29,35 @@ def test_load_tool_configuration(config_file):
         },
         "slug": "add",
         "version": "0.1.0",
+    } == normalized_tool_configuration
+
+
+def test_load_tool_configuration_templates(config_file_with_templates):
+    normalized_tool_configuration = cc.load_tool_configuration(
+        config_file_with_templates
+    )
+    assert {
+        "input": {
+            "templates": [
+                {"blocks": [{"id": "template-a"}], "id": "basic", "name": "Basic"}
+            ],
+            "variables": [
+                {"id": "a", "name": "A", "path": "numbers.json", "view": "number"},
+                {"id": "b", "name": "B", "path": "numbers.json", "view": "number"},
+            ],
+        },
+        "name": "Add Numbers",
+        "output": {
+            "templates": [
+                {
+                    "blocks": [{"data": {"value": "5"}, "view": "markdown"}],
+                    "id": "summary",
+                    "name": "Summary",
+                }
+            ],
+            "variables": [
+                {"id": "c", "name": "C", "path": "sum.json", "view": "number"}
+            ],
+        },
+        "version": {"name": "0.2.0"},
     } == normalized_tool_configuration
