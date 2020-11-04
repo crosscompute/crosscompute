865edfda23587f5cf4de0fcd7f6058fd2d0b4aca
Roy Hyunjin Han <rhh@crosscompute.com>
2020-11-03 21:45:45 -0500

    Revert to token

--- crosscompute/scripts/__init__.py
+++ crosscompute/scripts/__init__.py
@@ -1,7 +1,9 @@
 import sys
 from invisibleroads.scripts import LoggingScript, launch_script
 
-from ..routines import get_server_token, get_server_url
+from ..routines import (
+    get_server_url,
+    get_token)
 
 
 class AuthenticatingScript(LoggingScript):
@@ -9,7 +11,7 @@ class AuthenticatingScript(LoggingScript):
     def run(self, args, argv):
         super().run(args, argv)
         args.server_url = get_server_url()
-        args.server_token = get_server_token()
+        args.token = get_token()
 
 
 def launch(argv=sys.argv):
