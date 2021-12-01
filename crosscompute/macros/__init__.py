559b8968458ac40181f42701532e588fb3a00c0c
Kashfi <kashfi.fahim@gmail.com>
2021-12-01 17:09:06 +0000

    Demonstrate VIM macros

--- crosscompute/macros/__init__.py
+++ crosscompute/macros/__init__.py
@@ -2,7 +2,7 @@
 from .disk import is_path_in_folder, make_folder
 from .iterable import append_uniquely, extend_uniquely, find_item
 from .log import format_path
-from .macros import get_environment_value
+from .configuration import get_environment_value
 from .process import StoppableProcess
 from .text import compact_whitespace, normalize_key
 from .web import get_slug_from_name
