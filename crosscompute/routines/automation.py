f70dda3c174499ea39a6a0c1b98c0b859eddc224
Roy Hyunjin Han <rhh@crosscompute.com>
2021-12-08 18:16:49 +0000

    Run each automation

--- crosscompute/routines/automation.py
+++ crosscompute/routines/automation.py
@@ -31,17 +31,12 @@ class Automation():
     def initialize_from_path(self, configuration_path):
         configuration = load_configuration(configuration_path)
         configuration_folder = configuration['folder']
-        script_definition = configuration.get('script', {})
-        script_folder = script_definition.get('folder', '')
-        command_string = script_definition.get('command', '')
         automation_definitions = get_automation_definitions(
             configuration)
 
         self.configuration_path = configuration_path
         self.configuration = configuration
         self.configuration_folder = configuration_folder
-        self.script_folder = script_folder
-        self.command_string = command_string
         self.automation_definitions = automation_definitions
         self.automation_views = AutomationViews(automation_definitions)
         self.echo_views = EchoViews(configuration_folder)
@@ -68,63 +63,24 @@ class Automation():
         return instance
 
     def run(self, custom_environment=None):
-        if not self.command_string:
-            L.warning('command not defined in script configuration')
-            return
-        automation_definition = self.automation_definitions[0]
-        variable_definitions = get_raw_variable_definitions(
-            automation_definition, 'input')
-        # TODO: Load base custom environment from configuration
-        for batch_definition in automation_definition.get('batches', []):
-            batch_folder = prepare_batch_folder(
-                batch_definition, variable_definitions,
-                self.configuration_folder)
-            L.info(f'running {batch_folder}')
-            self.run_batch(batch_folder, custom_environment)
-
-    def run_batch(self, batch_folder, custom_environment=None):
-        input_folder = join(batch_folder, 'input')
-        output_folder = join(batch_folder, 'output')
-        log_folder = join(batch_folder, 'log')
-        debug_folder = join(batch_folder, 'debug')
-        self.run_script(
-            input_folder, output_folder, log_folder, debug_folder,
-            custom_environment)
-
-    def run_script(
-            self, input_folder, output_folder, log_folder, debug_folder,
-            custom_environment=None):
-        # TODO: Make each folder optional
-        default_environment = {
-            'CROSSCOMPUTE_INPUT_FOLDER': relpath(
-                input_folder, self.script_folder),
-            'CROSSCOMPUTE_OUTPUT_FOLDER': relpath(
-                output_folder, self.script_folder),
-            'CROSSCOMPUTE_LOG_FOLDER': relpath(
-                log_folder, self.script_folder),
-            'CROSSCOMPUTE_DEBUG_FOLDER': relpath(
-                debug_folder, self.script_folder),
-            'PATH': getenv('PATH', ''),
-        }
-        environment = default_environment | (custom_environment or {})
-        L.debug('environment = %s', environment)
-
-        for folder_label, relative_folder in {
-            'input': input_folder,
-            'output': output_folder,
-            'log': log_folder,
-            'debug': debug_folder,
-        }.items():
-            folder = make_folder(join(
-                self.configuration_folder, relative_folder))
-            L.info(f'{folder_label}_folder = {format_path(folder)}')
-
-        # TODO: Capture stdout and stderr for live output
-        subprocess.run(
-            self.command_string,
-            shell=True,
-            cwd=self.configuration_folder,
-            env=environment)
+        for automation_definition in self.automation_definitions:
+            script_definition = automation_definition.get('script', {})
+            command_string = script_definition.get('command')
+            if not command_string:
+                L.warning('command not defined in script configuration')
+                return
+            automation_folder = automation_definition['folder']
+            script_folder = script_definition.get('folder', '.')
+            variable_definitions = get_raw_variable_definitions(
+                automation_definition, 'input')
+            # TODO: Load base custom environment from configuration
+            for batch_definition in automation_definition.get('batches', []):
+                batch_folder = prepare_batch_folder(
+                    batch_definition, variable_definitions, automation_folder)
+                L.info(f'running {batch_folder}')
+                run_batch(
+                    batch_folder, command_string, script_folder,
+                    automation_folder, custom_environment)
 
     def serve(
             self,
@@ -180,3 +136,52 @@ class Automation():
             if not is_static:
                 config.include(self.echo_views.includeme)
         return config.make_wsgi_app()
+
+
+def run_batch(
+        batch_folder, command_string, script_folder, configuration_folder,
+        custom_environment=None):
+    input_folder = join(batch_folder, 'input')
+    output_folder = join(batch_folder, 'output')
+    log_folder = join(batch_folder, 'log')
+    debug_folder = join(batch_folder, 'debug')
+    run_script(
+        command_string, script_folder, input_folder, output_folder,
+        log_folder, debug_folder, configuration_folder,
+        custom_environment)
+
+
+def run_script(
+        command_string, script_folder, input_folder, output_folder,
+        log_folder, debug_folder, configuration_folder,
+        custom_environment=None):
+    # TODO: Make each folder optional
+    default_environment = {
+        'CROSSCOMPUTE_INPUT_FOLDER': relpath(
+            input_folder, script_folder),
+        'CROSSCOMPUTE_OUTPUT_FOLDER': relpath(
+            output_folder, script_folder),
+        'CROSSCOMPUTE_LOG_FOLDER': relpath(
+            log_folder, script_folder),
+        'CROSSCOMPUTE_DEBUG_FOLDER': relpath(
+            debug_folder, script_folder),
+        'PATH': getenv('PATH', ''),
+    }
+    environment = default_environment | (custom_environment or {})
+    L.debug('environment = %s', environment)
+
+    for folder_label, relative_folder in {
+        'input': input_folder,
+        'output': output_folder,
+        'log': log_folder,
+        'debug': debug_folder,
+    }.items():
+        folder = make_folder(join(configuration_folder, relative_folder))
+        L.info(f'{folder_label}_folder = {format_path(folder)}')
+
+    # TODO: Capture stdout and stderr for live output
+    subprocess.run(
+        command_string,
+        shell=True,
+        cwd=join(configuration_folder, script_folder),
+        env=environment)
