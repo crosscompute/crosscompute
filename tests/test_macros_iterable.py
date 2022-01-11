9ac6218265348e337a0e50554d861211c42dc46a
Roy Hyunjin Han <rhh@crosscompute.com>
2022-01-11 12:07:08 -0500

    Update test_macros_iterable.py

--- tests/test_macros_iterable.py
+++ tests/test_macros_iterable.py
@@ -2,7 +2,6 @@ from crosscompute.macros.iterable import extend_uniquely
 
 
 def test_extend_uniquely():
-    items = [1, 2, 3, 4, 5]
-    new_items = [1, 2, 33, 44, 52]
-    extend_uniquely(items, new_items)
-    assert len(items) == 8
+    items = [1, 2]
+    extend_uniquely(items, [2, 3])
+    assert len(items) == 3
