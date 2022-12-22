af66cf826c0887dfe372e4e4509377ea8d18d54c
Roy Hyunjin Han <rhh@crosscompute.com>
2022-12-22 15:37:30 +0000

    Get automation_slug in see_style

--- crosscompute/routers/root.py
+++ crosscompute/routers/root.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Path, Request
 from fastapi.responses import FileResponse
 
 from ..constants import (
-    IMAGES_FOLDER)
+    AUTOMATION_ROUTE,
+    IMAGES_FOLDER,
+    STYLE_ROUTE)
 from ..variables import (
     TemplateResponse,
     automation_definitions,
@@ -30,3 +32,17 @@ async def see_root(request: Request):
     tags=['root'])
 async def see_icon():
     return FileResponse(IMAGES_FOLDER / 'favicon.ico')
+
+
+@router.get(
+    STYLE_ROUTE,
+    tags=['root'])
+@router.get(
+    AUTOMATION_ROUTE + STYLE_ROUTE,
+    tags=['root'])
+# async def see_style(automation_slug: str | None = Path(default=None)):
+async def see_style(automation_slug: str | None = None):
+# async def see_style(automation_slug: Optional[str] = Path(default=None)):
+    print(automation_slug)
+    from fastapi import Response
+    return Response('')
