7c144e50ed8aa7ccc3ec92e562ce1b992566db47
Roy Hyunjin Han <rhh@crosscompute.com>
2023-01-14 13:07:00 -0500

    Fix race condition that refreshes before loading

--- crosscompute/templates/stringOutput.js
+++ crosscompute/templates/stringOutput.js
@@ -1,4 +1,4 @@
-registerElement('$variable_id', async function () {
+registerElement('$variable_id', async function() {
   try {
     await refreshString('$element_id', 'textContent', '$data_uri');
   } catch {
