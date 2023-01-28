00540f91fa946e29bda8c8d5fcb6d102db637d0c
Roy Hyunjin Han <rhh@crosscompute.com>
2023-01-28 07:33:44 -0500

    Save notes

--- crosscompute/routers/token.py
+++ crosscompute/routers/token.py
@@ -7,5 +7,8 @@ router = APIRouter()
 @router.post(
     'tokens.json',
     tags=['token'])
-async def add_token():
+async def add_token(
+#   authorization_guard: AuthorizationGuard = Depends(
+#       get_authorization_guard),
+):
     return {}
