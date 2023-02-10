c99881b0a06bbac93c8e2e20e56fd2339ff15597
Roy Hyunjin Han <rhh@crosscompute.com>
2023-02-09 21:07:02 -0500

    Simplify example

--- tests/log-variables/run.py
+++ tests/log-variables/run.py
@@ -1,20 +1,12 @@
 import json
-from os import getenv
 from pathlib import Path
-from time import sleep
+from sys import argv
+from time import sleep, time
 
 
-input_folder = Path(getenv(
-    'CROSSCOMPUTE_INPUT_FOLDER', 'batches/standard/input'))
-output_folder = Path(getenv(
-    'CROSSCOMPUTE_OUTPUT_FOLDER', 'batches/standard/output'))
-log_folder = Path(getenv(
-    'CROSSCOMPUTE_LOG_FOLDER', 'batches/standard/log'))
-debug_folder = Path(getenv(
-    'CROSSCOMPUTE_DEBUG_FOLDER', 'batches/standard/debug'))
-
-
+input_folder, output_folder, log_folder = [Path(_) for _ in argv[1:]]
 log_folder.mkdir(parents=True, exist_ok=True)
+t = time()
 
 
 with open(input_folder / 'variables.dictionary', 'rt') as f:
@@ -23,15 +15,17 @@ with open(input_folder / 'variables.dictionary', 'rt') as f:
     delay_in_seconds = d['delay_in_seconds']
 
 
-with open(log_folder / 'log.md', 'wt') as f:
+with open(
+    log_folder / 'info.md', 'wt',
+) as log_markdown_file:
     for i in range(iteration_count):
         print(i)
-        print(i, file=f)
-        f.flush()
+        print(i, file=log_markdown_file)
+        log_markdown_file.flush()
         sleep(delay_in_seconds)
 
 
 with open(output_folder / 'variables.dictionary', 'wt') as f:
     json.dump({
-        'time_in_seconds': iteration_count * delay_in_seconds,
+        'time_in_seconds': time() - t,
     }, f)
