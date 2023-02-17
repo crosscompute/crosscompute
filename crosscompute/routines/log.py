e26c1631a38decf03d21571030a7a6ab8ee97072
Roy Hyunjin Han <rhh@crosscompute.com>
2023-02-17 07:35:18 +0000

    Define timestamp argument

--- crosscompute/routines/log.py
+++ crosscompute/routines/log.py
@@ -9,13 +9,16 @@ def configure_argument_parser_for_logging(argument_parser):
     argument_parser.add_argument(
         '--verbose', '-v', dest='verbosity', action='count', default=0,
         help='increase logging level; stack as -vv')
+    argument_parser.add_argument(
+        '--timestamp', metavar='X', default='%Y%m%d-%H%M%S',
+        help='customize logging timestamp format')
 
 
 def configure_logging_from(args):
-    configure_logging(args.verbosity - args.quietness)
+    configure_logging(args.verbosity - args.quietness, args.timestamp)
 
 
-def configure_logging(intensity):
+def configure_logging(intensity, timestamp):
     logging_format = '%(asctime)s %(levelname)s %(message)s'
     if intensity >= 1:
         logging_level = DEBUG
@@ -31,6 +34,6 @@ def configure_logging(intensity):
     elif intensity <= -2:
         logging_level = CRITICAL
     basicConfig(
-        format=logging_format, datefmt='%Y%m%d-%H%M%S', level=logging_level)
+        format=logging_format, datefmt=timestamp, level=logging_level)
     if logging_level > DEBUG:
         getLogger('watchfiles').setLevel(ERROR)
