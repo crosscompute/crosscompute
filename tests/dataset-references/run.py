49d45af314ddbd02293a56499d5ce7261a2b1c1e
Roy Hyunjin Han <rhh@crosscompute.com>
2023-04-27 18:07:16 -0400

    Add dataset-references test

---
+++ tests/dataset-references/run.py
@@ -0,0 +1,17 @@
+import json
+import re
+from os import getenv
+from pathlib import Path
+
+
+output_folder = Path(getenv(
+    'CROSSCOMPUTE_OUTPUT_FOLDER', 'batches/standard/output'))
+datasets_folder = Path('datasets')
+document_path = datasets_folder / 'document.txt'
+with document_path.open('rt') as f:
+    text = re.sub(r'[^a-zA-Z ]', '', f.read())
+word_count = len(text.split())
+with (output_folder / 'variables.dictionary').open('wt') as f:
+    json.dump({
+        'word_count': word_count,
+    }, f)
