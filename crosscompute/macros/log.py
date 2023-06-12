711f7bb915b10e6ac0287a288b7571765230d83e
Roy Hyunjin Han <rhh@crosscompute.com>
2023-06-12 15:40:01 -0400

    Show logger name

--- crosscompute/macros/log.py
+++ crosscompute/macros/log.py
@@ -14,17 +14,19 @@ def configure_argument_parser_for_logging(argument_parser):
 def configure_logging_from(args, logging_level_by_package_name=None):
     with_debug = args.with_debug
     configure_logging(with_debug, '%Y%m%d-%H%M%S')
-    if with_debug:
-        return
-    for package_name, logging_level in logging_level_by_package_name.items():
-        getLogger(package_name).setLevel(logging_level)
+    if not with_debug:
+        for (
+            package_name, logging_level,
+        ) in logging_level_by_package_name.items():
+            getLogger(package_name).setLevel(logging_level)
 
 
 def configure_logging(with_debug, timestamp):
     if with_debug:
         logging_level = DEBUG
         logging_format = (
-            '%(asctime)s %(levelname)s %(module)s.%(funcName)s:%(lineno)s '
+            '%(asctime)s %(levelname)s '
+            '%(name)s.%(module)s.%(funcName)s:%(lineno)s '
             '%(message)s')
     else:
         logging_level = INFO
