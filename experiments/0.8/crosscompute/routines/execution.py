60a29cc17c05774574ba31320c05e3c5afa8622d
Roy Hyunjin Han <rhh@crosscompute.com>
2023-08-01 07:15:14 -0400

    Remove old code

--- experiments/0.8/crosscompute/routines/execution.py
+++ experiments/0.8/crosscompute/routines/execution.py
@@ -144,42 +144,8 @@ def run_tests(tool_definition):
     return d
 
 
-def prepare_dataset(file_path, file_view, project_dictionaries):
-    mime_type = get_mime_type(file_path)
-    dataset_dictionary = fetch_resource('datasets', method='POST', data={
-        'name': basename(file_path),
-        'view': file_view,
-        'type': mime_type,
-        'size': getsize(file_path),
-        'projects': project_dictionaries,
-    })
-    dataset_id = dataset_dictionary['id']
-    dataset_version_id = dataset_dictionary['version']['id']
-    file_dictionary = dataset_dictionary['file']
-    file_id = file_dictionary['id']
-    file_url = file_dictionary['url']
-    file_object = open(file_path, 'rb')
-    if file_path.endswith('json'):
-        d = sanitize_json_value(json.load(file_object))
-        file_object = StringIO(json.dumps(d))
-    requests.put(file_url, data=file_object)
-    fetch_resource('files', file_id, method='PATCH')
-    return {'id': dataset_id, 'version': {'id': dataset_version_id}}
-
-
 def prepare_variable_folder(
         folder, variable_definitions, variable_dictionaries):
-    value_by_id_by_path = defaultdict(dict)
-    variable_data_by_id = get_data_by_id(variable_dictionaries)
-    for variable_definition in variable_definitions:
-        variable_id = variable_definition['id']
-        try:
-            variable_data = variable_data_by_id[variable_id]
-        except KeyError:
-            raise CrossComputeDefinitionError({
-                'variable': f'could not find data for {variable_id}'})
-        file_path = join(folder, variable_definition['path'])
-        make_folder(dirname(file_path))
         if 'dataset' in variable_data:
             if not exists(file_path):
                 dataset_dictionary = variable_data['dataset']
@@ -197,24 +163,6 @@ def prepare_variable_folder(
             continue
         if 'value' not in variable_data:
             continue
-        variable_value = variable_data['value']
-        variable_view = variable_definition['view']
-        file_extension = splitext(file_path)[1]
-        save = define_save(variable_view, file_extension)
-        try:
-            save(file_path, variable_value, variable_id, value_by_id_by_path)
-        except ValueError:
-            raise CrossComputeExecutionError({
-                'path': 'is unsaveable by view ' + variable_view + file_path})
-        except CrossComputeError:
-            raise
-        except Exception:
-            print_exception(*exc_info())
-            raise CrossComputeImplementationError({'path': 'triggered an exception'})
-    for file_path, value_by_id in value_by_id_by_path.items():
-        file_extension = splitext(file_path)[1]
-        if file_extension == '.json':
-            save_json(file_path, value_by_id)
 
 
 def process_result_input_stream(script_command, is_quiet, as_json):
@@ -251,27 +199,6 @@ def process_result_input_stream(script_command, is_quiet, as_json):
 
 
 def get_result_name(result_dictionary):
-    variable_value_by_id = {}
-    for key in ['input', 'output']:
-        if key not in result_dictionary:
-            continue
-        put_dictionary = result_dictionary[key]
-        if 'variables' not in put_dictionary:
-            continue
-        for variable_definition in put_dictionary['variables']:
-            try:
-                variable_id = variable_definition['id']
-                variable_data = variable_definition['data']
-                if type(variable_data) is not dict:
-                    raise CrossComputeExecutionError({'definition': f'{variable_id} has a bad structure, it couldn\'t be batch'})
-
-                variable_value = variable_data['value']
-            except KeyError:
-                continue
-            if type(variable_value) not in [int, float, str]:
-                continue
-            variable_value_by_id[variable_id] = variable_value
-    raw_result_name = result_dictionary['name']
     result_name = raw_result_name.format_map(SafeDict(variable_value_by_id))
     return sanitize_name(result_name)
 
